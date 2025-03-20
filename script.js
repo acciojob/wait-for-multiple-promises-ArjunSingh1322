2d713a7fafbc07c496d9080b63bb9f9a3cd6d0bd
ArjunSingh1322 <as3388776@gmail.com>
2025-03-20 22:26:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,75 +1,49 @@
 
-// let table = document.queryselector("#output")
-
-// let p1 = new Promise((resolve, reject) => {
-//   let random = parseInt(Math.random() * 3 + 1);
-//   setTimeout(() => {
-//     resolve(random);
-//   }, random * 1000); 
-// });
-
-// let p2 = new Promise((resolve, reject) => {
-//   let random = parseInt(Math.random() * 3 + 1);
-//   setTimeout(() => {
-//     resolve(random);
-//   }, random * 1000);
-// });
-
-// let p3 = new Promise((resolve, reject) => {
-//   let random = parseInt(Math.random() * 3 + 1);
-//   setTimeout(() => {
-//     resolve(random);
-//   }, random * 1000);
-// });
-
-// Promise.all([p1, p2, p3]).then((res) => {
-//   let outputTable = document.getElementById("output");
-//   res.forEach((value, index) => {
-//     let tr = document.createElement("tr");
-//     let td1 = document.createElement("td");
-//     td1.innerText = `Promise ${index + 1}`;
-//     let td2 = document.createElement("td");
-//     td2.innerText = value;
-//     tr.append(td1,td2);
-//     table.append(tr)
-//   });
-// });
+let outputTable = document.getElementById("output");
 
 
+let loadingRow = document.createElement("tr");
+loadingRow.id = "loading"; 
+loadingRow.innerHTML = `<td colspan="2" class="text-center">Loading...</td>`;
+outputTable.appendChild(loadingRow);
 
 
+let p1 = new Promise((resolve) => {
+    let random = (Math.random() * 2 + 1).toFixed(3); 
+    setTimeout(() => {
+        resolve({ name: "Promise 1", timeTaken: random });
+    }, random * 1000);
+});
+
+let p2 = new Promise((resolve) => {
+    let random = (Math.random() * 2 + 1).toFixed(3);
+    setTimeout(() => {
+        resolve({ name: "Promise 2", timeTaken: random });
+    }, random * 1000);
+});
+
+let p3 = new Promise((resolve) => {
+    let random = (Math.random() * 2 + 1).toFixed(3);
+    setTimeout(() => {
+        resolve({ name: "Promise 3", timeTaken: random });
+    }, random * 1000);
+});
 
 
+Promise.all([p1, p2, p3]).then((results) => {
+   
+	loadingRow.remove();
+    
+    
+    let totalTime = Math.max(...results.map(p => parseFloat(p.timeTaken))).toFixed(3);
+    results.forEach(result => {
+        let row = document.createElement("tr");
+        row.innerHTML = `<td>${result.name}</td><td>${result.timeTaken}</td>`;
+        outputTable.appendChild(row);
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    let totalRow = document.createElement("tr");
+    totalRow.innerHTML = `<td><strong>Total</strong></td><td><strong>${totalTime}</strong></td>`;
+    outputTable.appendChild(totalRow);
+});
