3745a3bf02b69315e2e3ecc251b7bba2c3a8ea08
ArjunSingh1322 <as3388776@gmail.com>
2025-03-20 21:27:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,75 @@
-//your JS code here. If required.
+
+// let table = document.queryselector("#output")
+
+// let p1 = new Promise((resolve, reject) => {
+//   let random = parseInt(Math.random() * 3 + 1);
+//   setTimeout(() => {
+//     resolve(random);
+//   }, random * 1000); 
+// });
+
+// let p2 = new Promise((resolve, reject) => {
+//   let random = parseInt(Math.random() * 3 + 1);
+//   setTimeout(() => {
+//     resolve(random);
+//   }, random * 1000);
+// });
+
+// let p3 = new Promise((resolve, reject) => {
+//   let random = parseInt(Math.random() * 3 + 1);
+//   setTimeout(() => {
+//     resolve(random);
+//   }, random * 1000);
+// });
+
+// Promise.all([p1, p2, p3]).then((res) => {
+//   let outputTable = document.getElementById("output");
+//   res.forEach((value, index) => {
+//     let tr = document.createElement("tr");
+//     let td1 = document.createElement("td");
+//     td1.innerText = `Promise ${index + 1}`;
+//     let td2 = document.createElement("td");
+//     td2.innerText = value;
+//     tr.append(td1,td2);
+//     table.append(tr)
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
